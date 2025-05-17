7dd1deb9a2a6c44f42fbdefd9d26d55c3714576b
Zahar BlackSystem <zaharb840@gmail.com>
2025-05-17 15:37:33 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+if [ "$EUID" -ne 0 ]; then 
+  NONROOT=1
+else 
+  NONROOT=0
+fi
 gcc fe/src/fe.c feb.c -Ife/src -o feb.bootstrap
 ./feb.bootstrap build.fe
-if [ "$EUID" -ne 0 ]; then 
-  echo "feb builded, but the script is not run as root. if you want to install feb and add it to /bin - restart it as root."
-  exit 1
+read -p "Do you want to install feb in your system? [Y/n] " -r yn
+if [[ $yn == [Nn]* ]]; then 
+      exit 1
+else
+if [ "$NONROOT" -eq 1  ]; then
+if command -v sudo &> /dev/null; then
+  sudo cp ./feb /bin/
+else
+  doas cp ./feb /bin/
 fi
-cp ./feb /bin/
+fi
