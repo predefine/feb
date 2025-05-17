2edf0f48c81445c3f83b585fd07d327576c69d97
Zahar BlackSystem <zaharb840@gmail.com>
2025-05-17 15:47:59 +0300

    Update and rename build.sh to bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+  NONROOT=1
+else
+  NONROOT=0
+fi
+
+gcc fe/src/fe.c feb.c -Ife/src -o feb.bootstrap
+./feb.bootstrap build.fe
+
+read -p "Do you want to install feb in your system? [Y/n] " -r yn
+if [[ $yn == [Nn]* ]]; then
+  exit 1
+else
+  if [ "$NONROOT" -eq 1 ]; then
+    if command -v sudo &> /dev/null; then
+      sudo cp ./feb /bin/
+    else
+      doas cp ./feb /bin/
+    fi
+  fi
+fi
